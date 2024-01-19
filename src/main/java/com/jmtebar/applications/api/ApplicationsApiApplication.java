3d814c05294d217f9c1b049c36e4653797a61844
Jose Miguel Tebar <josemiguel.tebar@onlyfy.com>
2024-01-19 14:32:00 +0100

    Initial commit

---
+++ src/main/java/com/jmtebar/applications/api/ApplicationsApiApplication.java
@@ -0,0 +1,13 @@
+package com.jmtebar.applications.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmtebar/applications/api/ApplicationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmtebar.applications.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
